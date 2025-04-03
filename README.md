464709b22d45128886c1108d66b97f32b988658c
akbar <akbarsmp27@gmail.com>
2025-04-04 01:12:42 +0700

    ready to publish to public repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gummypy
+
+A small library that contains wrappper functions around the [gum](https://github.com/charmbracelet/gum) CLI tool.
---
+++ src/gummypy/__init__.py
@@ -0,0 +1,32 @@
+from gum import choose
+from gum import confirm
+from gum import file
+
+# from gum import filter
+from gum import format
+from gum import input
+from gum import join
+from gum import pager
+from gum import spin
+
+# from gum import style
+from gum import table
+from gum import write
+from gum import log
+from gum import version_check
+
+__all__ = [
+    "choose",
+    "confirm",
+    "file",
+    "format",
+    "input",
+    "join",
+    "pager",
+    "spin",
+    "table",
+    "write",
+    "log",
+    "version_check",
+]
+
