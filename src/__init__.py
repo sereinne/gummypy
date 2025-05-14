6fbad2422c226c2e5d3748c946b4c7e72229491b
sereinne <akbarpersonalmode@gmail.com>
2025-05-15 00:05:52 +0700

    renew __init__.py and pyproject.toml.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,31 @@
-from .gummypy import choose, confirm, file, format, input, join, pager, spin, table, write, log
+from .gummypy import GumCommandBuilder, GumWrappers
 
-__version__ = "0.1.2"
-__all__ = [ "choose", "confirm", "file", "format", "input", "join", "pager", "spin", "table", "write", "log" ]
+__version__ = "1.0.0"
+
+choose = GumWrappers.choose
+confirm = GumWrappers.confirm
+file = GumWrappers.file
+format = GumWrappers.format
+input = GumWrappers.input
+join = GumWrappers.join
+pager = GumWrappers.pager
+spin = GumWrappers.spin
+table = GumWrappers.table
+write = GumWrappers.write
+log = GumWrappers.log
+
+__all__ = [
+    "GumCommandBuilder",
+    "GumWrappers",
+    "choose",
+    "confirm",
+    "file",
+    "format",
+    "input",
+    "join",
+    "pager",
+    "spin",
+    "table",
+    "write",
+    "log",
+]
